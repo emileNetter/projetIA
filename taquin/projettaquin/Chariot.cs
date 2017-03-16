586e1dc56114274e2daefc413919b7026c0f5ce7
emileNetter <emile@netter.fr>
2017-03-16 10:43:16 +0100

    Classe Chariot et Objet

---
+++ taquin/projettaquin/Chariot.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projettaquin
+{
+    class Chariot
+    {
+        private int posX { get; set; }
+        private int posY { get; set; }
+        private enum orientation { nord,sud};
+        private int hauteur;
+
+        public Chariot (int _posX, int _posY)
+        {
+            posX = _posX;
+            posY = _posY;
+        }
+
+    }
+}
---
+++ taquin/projettaquin/Objet.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projettaquin
+{
+
+    class Objet
+    {
+        private int posX { get; set; }
+        private int posY { get; set; }
+        public enum Orientation { Nord, Sud };
+        public Orientation orientation { get; set; }
+        private int hauteur { get; set; }
+
+        public Objet(int posX, int posY, Orientation orientation, int hauteur )
+        {
+            this.posX = posX;
+            this.posY = posY;
+            this.orientation = orientation;
+            this.hauteur = hauteur;
+        }
+    }
+}
