971eea94b2e194bdf919ddb63c608b84777a260c
arthur aubin <arthur aubin>
2017-03-16 10:38:20 +0100

    création du tableau

--- taquin/projettaquin/NodeEnrepot.cs
+++ taquin/projettaquin/NodeEnrepot.cs
@@ -8,5 +8,23 @@ namespace projettaquin
 {
     class NodeEnrepot:GenericNode
     {
+        public static int[,] tabEntrepot = new int[25, 25];
+        int NBC;
+
+        void InitialiserEntrepot()
+        {
+            for(int i=2; i<22; i+=2)
+            {
+
+                for (int premiereLigne = 4; premiereLigne < 13; premiereLigne++) { tabEntrepot[premiereLigne, i] = -1; }
+                for (int deuxiemeLigne = 16; deuxiemeLigne < 25; deuxiemeLigne++) { tabEntrepot[deuxiemeLigne, i] = -1; }
+
+            }
+
+            for(int j=0; j<25;j++)
+            {
+                tabEntrepot[0, j] = -2;
+            }
+        }
     }
 }
