c148cbab7cd3f40c948819fb98536e3d19320935
Arthur Aubin <aaubin003@ensc.fr>
2017-03-22 15:23:48 +0100

    c'est bon ça bouge plus

--- taquin/projettaquin/FormViewGlobal.Designer.cs
+++ taquin/projettaquin/FormViewGlobal.Designer.cs
@@ -95,13 +95,14 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
-            this.ClientSize = new System.Drawing.Size(924, 602);
+            this.ClientSize = new System.Drawing.Size(734, 461);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.numericUpDown1);
             this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "FormViewGlobal";
             this.Text = "FormViewGlobal";
