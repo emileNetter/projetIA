b4ab6981dc85f018c39023e88bcb2cfb3b5dd35d
thocasado <thocasado@ensc.fr>
2017-03-31 10:11:04 +0200

    Amelioration View, possibilité de relancer simulation avec bouton Nouveau

--- taquin/projettaquin/FormViewGlobal.Designer.cs
+++ taquin/projettaquin/FormViewGlobal.Designer.cs
@@ -47,14 +47,18 @@
             this.button2 = new System.Windows.Forms.Button();
             this.comboBoxAleatoire = new System.Windows.Forms.ComboBox();
             this.btn_LancerSimulation = new System.Windows.Forms.Button();
+            this.button_nouveau = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label6 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(874, 24);
+            this.label1.Location = new System.Drawing.Point(124, -15);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(125, 17);
@@ -63,7 +67,7 @@
             // 
             // numericUpDown1
             // 
-            this.numericUpDown1.Location = new System.Drawing.Point(1009, 24);
+            this.numericUpDown1.Location = new System.Drawing.Point(152, 27);
             this.numericUpDown1.Margin = new System.Windows.Forms.Padding(4);
             this.numericUpDown1.Name = "numericUpDown1";
             this.numericUpDown1.Size = new System.Drawing.Size(160, 22);
@@ -72,7 +76,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(877, 90);
+            this.button1.Location = new System.Drawing.Point(13, 57);
             this.button1.Margin = new System.Windows.Forms.Padding(4);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(171, 28);
@@ -83,7 +87,7 @@
             // 
             // btn_valider
             // 
-            this.btn_valider.Location = new System.Drawing.Point(971, 485);
+            this.btn_valider.Location = new System.Drawing.Point(107, 439);
             this.btn_valider.Margin = new System.Windows.Forms.Padding(4);
             this.btn_valider.Name = "btn_valider";
             this.btn_valider.Size = new System.Drawing.Size(158, 65);
@@ -94,7 +98,7 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(876, 208);
+            this.button3.Location = new System.Drawing.Point(12, 162);
             this.button3.Margin = new System.Windows.Forms.Padding(4);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(161, 28);
@@ -106,7 +110,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(874, 168);
+            this.label2.Location = new System.Drawing.Point(10, 122);
             this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(24, 17);
@@ -116,7 +120,7 @@
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(877, 126);
+            this.comboBox1.Location = new System.Drawing.Point(13, 93);
             this.comboBox1.Margin = new System.Windows.Forms.Padding(4);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(169, 24);
@@ -125,7 +129,7 @@
             // comboBoxManuel
             // 
             this.comboBoxManuel.FormattingEnabled = true;
-            this.comboBoxManuel.Location = new System.Drawing.Point(876, 244);
+            this.comboBoxManuel.Location = new System.Drawing.Point(12, 198);
             this.comboBoxManuel.Margin = new System.Windows.Forms.Padding(4);
             this.comboBoxManuel.Name = "comboBoxManuel";
             this.comboBoxManuel.Size = new System.Drawing.Size(160, 24);
@@ -134,7 +138,7 @@
             // 
             // textBoxX
             // 
-            this.textBoxX.Location = new System.Drawing.Point(1107, 211);
+            this.textBoxX.Location = new System.Drawing.Point(243, 165);
             this.textBoxX.Margin = new System.Windows.Forms.Padding(4);
             this.textBoxX.Name = "textBoxX";
             this.textBoxX.Size = new System.Drawing.Size(55, 22);
@@ -142,7 +146,7 @@
             // 
             // textBoxY
             // 
-            this.textBoxY.Location = new System.Drawing.Point(1107, 246);
+            this.textBoxY.Location = new System.Drawing.Point(243, 200);
             this.textBoxY.Margin = new System.Windows.Forms.Padding(4);
             this.textBoxY.Name = "textBoxY";
             this.textBoxY.Size = new System.Drawing.Size(55, 22);
@@ -151,7 +155,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(1077, 214);
+            this.label3.Location = new System.Drawing.Point(213, 168);
             this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(22, 17);
@@ -161,7 +165,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(1076, 251);
+            this.label4.Location = new System.Drawing.Point(212, 205);
             this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(23, 17);
@@ -170,7 +174,7 @@
             // 
             // btn_ValiderPos
             // 
-            this.btn_ValiderPos.Location = new System.Drawing.Point(1196, 208);
+            this.btn_ValiderPos.Location = new System.Drawing.Point(332, 162);
             this.btn_ValiderPos.Margin = new System.Windows.Forms.Padding(4);
             this.btn_ValiderPos.Name = "btn_ValiderPos";
             this.btn_ValiderPos.Size = new System.Drawing.Size(100, 57);
@@ -190,7 +194,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(874, 326);
+            this.label5.Location = new System.Drawing.Point(10, 280);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(111, 17);
             this.label5.TabIndex = 14;
@@ -198,7 +202,7 @@
             // 
             // numericUpDown2
             // 
-            this.numericUpDown2.Location = new System.Drawing.Point(997, 326);
+            this.numericUpDown2.Location = new System.Drawing.Point(133, 280);
             this.numericUpDown2.Minimum = new decimal(new int[] {
             1,
             0,
@@ -215,7 +219,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(877, 370);
+            this.button2.Location = new System.Drawing.Point(13, 324);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(166, 23);
             this.button2.TabIndex = 16;
@@ -226,7 +230,7 @@
             // comboBoxAleatoire
             // 
             this.comboBoxAleatoire.FormattingEnabled = true;
-            this.comboBoxAleatoire.Location = new System.Drawing.Point(877, 399);
+            this.comboBoxAleatoire.Location = new System.Drawing.Point(13, 353);
             this.comboBoxAleatoire.Name = "comboBoxAleatoire";
             this.comboBoxAleatoire.Size = new System.Drawing.Size(311, 24);
             this.comboBoxAleatoire.TabIndex = 17;
@@ -234,44 +238,77 @@
             // btn_LancerSimulation
             // 
             this.btn_LancerSimulation.Enabled = false;
-            this.btn_LancerSimulation.Location = new System.Drawing.Point(971, 613);
+            this.btn_LancerSimulation.Location = new System.Drawing.Point(221, 574);
             this.btn_LancerSimulation.Name = "btn_LancerSimulation";
             this.btn_LancerSimulation.Size = new System.Drawing.Size(158, 65);
             this.btn_LancerSimulation.TabIndex = 18;
             this.btn_LancerSimulation.Text = "Lancer simulation";
             this.btn_LancerSimulation.UseVisualStyleBackColor = true;
             // 
+            // button_nouveau
+            // 
+            this.button_nouveau.Location = new System.Drawing.Point(1067, 142);
+            this.button_nouveau.Name = "button_nouveau";
+            this.button_nouveau.Size = new System.Drawing.Size(143, 36);
+            this.button_nouveau.TabIndex = 19;
+            this.button_nouveau.Text = "Nouveau";
+            this.button_nouveau.UseVisualStyleBackColor = true;
+            this.button_nouveau.Visible = false;
+            this.button_nouveau.Click += new System.EventHandler(this.button_nouveau_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.btn_LancerSimulation);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.textBoxX);
+            this.groupBox1.Controls.Add(this.numericUpDown1);
+            this.groupBox1.Controls.Add(this.comboBoxAleatoire);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.btn_valider);
+            this.groupBox1.Controls.Add(this.numericUpDown2);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.btn_ValiderPos);
+            this.groupBox1.Controls.Add(this.comboBoxManuel);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.textBoxY);
+            this.groupBox1.Location = new System.Drawing.Point(855, 34);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(442, 522);
+            this.groupBox1.TabIndex = 20;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(10, 29);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(137, 17);
+            this.label6.TabIndex = 19;
+            this.label6.Text = "Nombre de charriots";
+            // 
             // FormViewGlobal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1309, 690);
-            this.Controls.Add(this.btn_LancerSimulation);
-            this.Controls.Add(this.comboBoxAleatoire);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.numericUpDown2);
-            this.Controls.Add(this.label5);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button_nouveau);
             this.Controls.Add(this.splitter1);
-            this.Controls.Add(this.btn_ValiderPos);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.textBoxY);
-            this.Controls.Add(this.textBoxX);
-            this.Controls.Add(this.comboBoxManuel);
-            this.Controls.Add(this.comboBox1);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.btn_valider);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.numericUpDown1);
-            this.Controls.Add(this.label1);
             this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "FormViewGlobal";
             this.Text = "FormViewGlobal";
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -296,5 +333,8 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.ComboBox comboBoxAleatoire;
         private System.Windows.Forms.Button btn_LancerSimulation;
+        private System.Windows.Forms.Button button_nouveau;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label6;
     }
 }
--- taquin/projettaquin/FormViewGlobal.cs
+++ taquin/projettaquin/FormViewGlobal.cs
@@ -198,6 +198,7 @@ namespace projettaquin
             t.calculeTemps(); // on calcule le temps mis pour ce chemin
 
             setViewEntrepot();
+            reinitialiserView();
             if (tabChariot.Length != 0 && tabObjet.Length !=0) { btn_LancerSimulation.Enabled = true; }
         }
 
@@ -241,5 +242,42 @@ namespace projettaquin
                 comboBoxAleatoire.Items.Add("Objet : x=" + o.posX + " y=" + o.posY + " orientation=" + o.orientation);
             }
         }
+
+
+        private void reinitialiserView()
+        {
+            /*foreach (Control l in this.Controls.OfType<Control>())
+            {
+                if(l is Label)
+                {
+                    if(l.Name == "label1" || l.Name == "label2" || l.Name == "label3" || l.Name == "label4")
+                    {
+                        l.Visible = false;                    }
+                }
+                else l.Visible = false;
+            }*/
+            groupBox1.Visible = false;
+            button_nouveau.Visible = true;
+        }
+
+        private void button_nouveau_Click(object sender, EventArgs e)
+        {
+            /*foreach (Control l in this.Controls.OfType<Control>())
+            {
+                if (l is Label)
+                {
+                    if (l.Name == "label1" || l.Name == "label2" || l.Name == "label3" || l.Name == "label4")
+                    {
+                        l.Visible = true;
+                    }
+                }
+                else l.Visible = true;
+            }*/
+            btn_valider.Enabled = true;
+            groupBox1.Visible = true;
+            button_nouveau.Visible = false;
+        }
+
+        
     }
 }
