ff3f5cb94fc489923a8c4704003aea92c8351b6f
emileNetter <emile@netter.fr>
2017-04-07 12:03:35 +0200

    Read from file

--- taquin/Classification/Perceptron.cs
+++ taquin/Classification/Perceptron.cs
@@ -3,12 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Classification
 {
     class Perceptron
     {
         private double[,] inputs = new double[3000, 3];
+
+        private void readFromFile()
+        {
+            using(StreamReader sr = new StreamReader("datasetclassif.txt"))
+            {
+                for(int i =0; i < inputs.GetLength(0); i++)
+                {
+                    for (int j=0; j < inputs.GetLength(1); j++)
+                    {
+                        // Read the stream to a string, and write the string to the console.
+                        inputs[i,j] = Convert.ToDouble(sr.ReadToEnd());                      
+                    }
+                }               
+            }
+        } 
 
     }
 }
